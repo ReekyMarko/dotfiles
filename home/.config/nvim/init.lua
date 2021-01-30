1fe9f7a702afc6b75b9271b049efdc71aaafa726
Marko Korhonen <marko@korhonen.cc>
2021-01-30 23:18:17 +0200

    Re-enable airline

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -4,7 +4,7 @@ require 'settings'
 require 'pluginmanager'
 
 -- Plugin configurations
---require 'pluginconf.airline'
+require 'pluginconf.airline'
 require 'pluginconf.completion'
 require 'pluginconf.lsp'
 require 'pluginconf.treesitter'
