0d2ac08fc74861212c5e2b52cfd6ac8b38a9aa14
Marko Korhonen <marko@korhonen.cc>
2021-02-02 19:06:55 +0200

    Fix YAML indentation

--- home/.config/nvim/lua/autocmd.lua
+++ home/.config/nvim/lua/autocmd.lua
@@ -2,3 +2,6 @@ local cmd = vim.cmd
 
 -- Remap exit terminal mode to esc
 cmd('au TermOpen * tnoremap <buffer> <Esc> <c-\\><c-n>')
+
+-- Fix YAML indentation
+cmd('au FileType yaml setlocal ts=2 sts=2 sw=2 expandtab')
